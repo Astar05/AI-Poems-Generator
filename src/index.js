5af96cee29aa431aa0be7ed5d736ee35e20d5794
Astar05 <nhuynh78@gmail.com>
2024-02-18 00:24:52 -0500

    created Haiku function with API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The poem will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a Haiku expert and enjoys to write them. Your mission is to generate a haiku; no more than 17 syllables, composed of 3 lines; after each line there's a <br /> - every first line has 5 syllables, second line has 7 syllables, and the third line has 5 syllables.";
+
+  let prompt = `User instructions: Generate a Haiku about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Haiku about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
